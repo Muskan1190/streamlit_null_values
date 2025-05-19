63a27f5b3f3b718efbec0f0ebe0d5859a55d0073
Muskan Patel <93847337+Muskan1190@users.noreply.github.com>
2025-05-19 01:05:35 -0500

    Add files via upload

---
+++ experiment.py
@@ -0,0 +1,28 @@
+import streamlit as st
+import pandas as pd
+
+
+
+st.title("🧹 Fill Null Values")
+
+# Match your original variable name
+filee = st.file_uploader("Choose a CSV file", type=["csv"])
+
+if filee is not None:
+    file = pd.read_csv(filee)  # Use your variable name `file`
+    st.subheader("📋 Original Data")
+    st.dataframe(file)
+    
+    option = st.selectbox(
+        "Fill with",
+        ("Mean","Minimum","Mode"))
+
+    if(st.button("Generate")):
+        if (option =="Mean"):
+            file.fillna((file.mean()),inplace = True)
+        if (option =="Min"):
+            file.fillna((file.min()),inplace = True)
+        if (option =="Mode"):
+            file.fillna((file.mode()),inplace = True)
+        
+    st.dataframe(file)
